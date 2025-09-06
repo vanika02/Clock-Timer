3a4bdd196c7214b1273462fe306f99a937a7838d
vanika02 <vanikadangi02@gmail.com>
2025-09-06 02:49:25 +0000

    added ist clock and alarm sound

--- app.py
+++ app.py
@@ -1,32 +1,53 @@
 import tkinter as tk
 from tkinter import messagebox
 import time
+from datetime import datetime
+from zoneinfo import ZoneInfo
+import pygame
+
 
 # Tk() to create the main window in tkinter, we use the Tk class
-
 root = tk.Tk()
 root.protocol("WM_DELETE_WINDOW", root.destroy) # ensure process ends 
 root.minsize(600, 600)
+
+# initializing pygame and pygame mixer
+pygame.init()
+
+
 
 root.title("TIMER CLOCK")
 
 # Creating a label 
 tk.Label(root, text="TIMER CLOCK").pack()
 
-def start_clock():
-    # get the time in seconds
-    start = time.perf_counter()
-    for i in range(10000):
-        i = i+1
-    end = time.perf_counter()
-    print(end-start)
+
+# creating a clock using canvas
+
+def digital_Clock():
+    ist_time = datetime.now(ZoneInfo("Asia/Kolkata"))
+    cm_time = ist_time.strftime("%Y-%m-%D %H:%M:%S")
+    day, timez = cm_time.split(' ')
+    lbl.config(text=timez)
+    lbl.after(1000, digital_Clock)
+
+lbl = tk.Label(root, font=('calibri', 40, 'bold'), background='black', foreground='white')
+lbl.pack(anchor='center', fill='x')
+digital_Clock()
+
 
 def timer_completed():
+    pygame.mixer.init()
+    pygame.mixer.music.load("alarm.wav")
+    pygame.mixer.music.play()
     messagebox.showerror("Done", "Time is completed")
+    time.sleep(6) # time sleep for 3 seconds
+    pygame.mixer.music.stop()
+    pygame.quit()
 
 # creating start and end buttons 
 input_Value = tk.Entry(root)
-input_Value.pack()
+input_Value.pack(padx=20, pady=20, ipadx=10, ipady=10, fill='x')
 
 print(time.time())
 def get_input_value():
@@ -41,14 +62,7 @@ def get_input_value():
     root.after(num_sec, timer_completed)
 
 
-input_Value_button = tk.Button(root, text="ENTER TIME IN SECONDS", command=get_input_value).pack(padx=5, pady=5, fill='x') 
-
-
-
-
-
-
-
+input_Value_button = tk.Button(root, text="ENTER TIME IN SECONDS", command=get_input_value, width=20, height=2).pack(padx=5, pady=5, ipadx=10, ipady=10, fill='x') 
 
 
 
